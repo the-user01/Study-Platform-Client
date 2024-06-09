43a6a35b3b6d98b736ef640e7b4de9bc5a489f83
the-user01 <sakibcom02@gmail.com>
2024-06-09 23:21:18 +0600

    connected to vercel server

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://study-platform-server.vercel.app'
 })
 
 const useAxiosSecure = () => {
