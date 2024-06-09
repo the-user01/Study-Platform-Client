b6479c32d3be9b916e129a0a06000f0260973f07
the-user01 <sakibcom02@gmail.com>
2024-06-09 16:27:41 +0600

    added tanstact query

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { HelmetProvider } from 'react-helmet-async'
 import AuthProvider from './provider/AuthProvider'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/Dashboard/admin/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/admin/AllUsers/AllUsers.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import DashboardHelmet from '../../../../hooks/DashboardHelmet';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 
 const AllUsers = () => {
+    const axiosSecure = useAxiosSecure();
+
+
+    const {data: users = []} = useQuery({
+        queryKey: ['users'],
+        queryFn: async() =>{
+            const res = await axiosSecure.get('/users');
+            return res.data
+        }
+    })
+
+
     return (
         <>
         <DashboardHelmet name='All Users'></DashboardHelmet>
+
+        <div>
+            <div className='flex justify-evenly my-4'>
+                <h2 className="text-3xl">All Users</h2>
+                <h2 className="text-3xl"> Total Users: {users.length}</h2>
+            </div>
+        </div>
         
-        Showing All Users Besh
         </>
     );
 };
