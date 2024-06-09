1054aaa4d81b0d54d7d027eb0511acaaabe22733
the-user01 <sakibcom02@gmail.com>
2024-06-09 17:04:24 +0600

    added delete function

--- src/pages/Dashboard/admin/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/admin/AllUsers/AllUsers.jsx
@@ -2,31 +2,104 @@ import React from 'react';
 import DashboardHelmet from '../../../../hooks/DashboardHelmet';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
 
 
-    const {data: users = []} = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
-        queryFn: async() =>{
+        queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data
         }
     })
 
+    const handleDeleteUser = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: `${user.name} has been deleted.`,
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
+    const handleSearch = () => {
+        // console.log("Something");
+    }
+
 
     return (
         <>
-        <DashboardHelmet name='All Users'></DashboardHelmet>
+            <DashboardHelmet name='All Users'></DashboardHelmet>
 
-        <div>
-            <div className='flex justify-evenly my-4'>
-                <h2 className="text-3xl">All Users</h2>
-                <h2 className="text-3xl"> Total Users: {users.length}</h2>
+            <div>
+                <div className='flex justify-evenly my-4'>
+                    <h2 className="text-3xl">All Users</h2>
+
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input type="text" className="grow" placeholder="Search User" />
+                        <button onClick={handleSearch}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                        </button>
+                    </label>
+                </div>
             </div>
-        </div>
-        
+
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, idx) => <tr key={user._id}>
+                                    <th>{idx + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.role}</td>
+                                    <td>
+                                        <button
+                                            onClick={() => handleDeleteUser(user)}
+                                            className='btn btn-ghost'>
+                                            <FaTrashAlt className='text-red-600'></FaTrashAlt>
+                                        </button>
+                                    </td>
+                                </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </>
     );
 };
