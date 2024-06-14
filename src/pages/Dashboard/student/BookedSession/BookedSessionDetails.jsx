f5b2374c506d67c26e64377c8eb730ced937b258
the-user01 <sakibcom02@gmail.com>
2024-06-14 11:58:30 +0600

    updated codes

--- src/pages/Dashboard/student/BookedSession/BookedSessionDetails.jsx
+++ src/pages/Dashboard/student/BookedSession/BookedSessionDetails.jsx
@@ -9,7 +9,7 @@ const BookedSessionDetails = () => {
     const axiosSecure = useAxiosSecure();
 
     // fetching booked session data
-    const { data: bookedDetailsSessions=[], isPending: isBookedSessionLoading } = useQuery({
+    const { data: bookedDetailsSessions = [], isPending: isBookedSessionLoading } = useQuery({
         queryKey: ['bookedDetailsSessions'],
         queryFn: async () => {
             // load pending sessions
@@ -18,61 +18,64 @@ const BookedSessionDetails = () => {
         },
     })
 
-    console.log(bookedDetailsSessions);
-
-
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content">
                     <div>
+                        {
+                            !isBookedSessionLoading ?
+                                <>
+                                    {/* title */}
+                                    < h1 className="text-5xl font-bold">{bookedDetailsSessions.sessionTitle}</h1>
+                                    <p className="py-6">{bookedDetailsSessions.description}</p>
 
-                        {/* title */}
-                        <h1 className="text-5xl font-bold">{bookedDetailsSessions.sessionTitle}</h1>
-                        <p className="py-6">{bookedDetailsSessions.description}</p>
+                                    <div className="divider"></div>
 
-                        <div className="divider"></div>
+                                    {/* Registration info */}
+                                    <div className="flex flex-col md:flex-row justify-between gap-12">
+                                        <h2>
+                                            <b>Registration Start date: </b> {bookedDetailsSessions.regStartDate}
+                                        </h2>
 
-                        {/* Registration info */}
-                        <div className="flex flex-col md:flex-row justify-between gap-12">
-                            <h2>
-                                <b>Registration Start date: </b> {bookedDetailsSessions.regStartDate}
-                            </h2>
+                                        <h2>
+                                            <b>Registration End date: </b> {bookedDetailsSessions.regEndDate}
+                                        </h2>
+                                    </div>
 
-                            <h2>
-                                <b>Registration End date: </b> {bookedDetailsSessions.regEndDate}
-                            </h2>
-                        </div>
+                                    <div className="divider"></div>
 
-                        <div className="divider"></div>
+                                    {/* Class time Info */}
+                                    <div className="flex flex-col md:flex-row justify-between gap-12">
+                                        <h2>
+                                            <b>Class Start date: </b> {bookedDetailsSessions.classStartDate}
+                                        </h2>
 
-                        {/* Class time Info */}
-                        <div className="flex flex-col md:flex-row justify-between gap-12">
-                            <h2>
-                                <b>Class Start date: </b> {bookedDetailsSessions.classStartDate}
-                            </h2>
+                                        <h2>
+                                            <b>Class End date: </b> {bookedDetailsSessions.classEndDate}
+                                        </h2>
+                                    </div>
 
-                            <h2>
-                                <b>Class End date: </b> {bookedDetailsSessions.classEndDate}
-                            </h2>
-                        </div>
+                                    <div className="divider"></div>
 
-                        <div className="divider"></div>
+                                    {/* tutor & session info */}
+                                    <div className="flex flex-col md:flex-row justify-between gap-12">
+                                        <h2>
+                                            <b>Session Duration: </b> {bookedDetailsSessions.sessionDuration} hrs
+                                        </h2>
 
-                        {/* tutor & session info */}
-                        <div className="flex flex-col md:flex-row justify-between gap-12">
-                            <h2>
-                                <b>Session Duration: </b> {bookedDetailsSessions.sessionDuration} hrs
-                            </h2>
+                                        <h2>
+                                            <b>Tutor Name: </b> {bookedDetailsSessions.tutorName}
+                                        </h2>
+                                    </div>
+                                </> :
+                                <span className="loading loading-dots loading-lg"></span>
+                        }
 
-                            <h2>
-                                <b>Tutor Name: </b> {bookedDetailsSessions.tutorName}
-                            </h2>
-                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
